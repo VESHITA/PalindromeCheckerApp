a9ab125591e61a2865dd82d30a748b6113ee0bec
VESHITA <ev8105@srmist.edu.in>
2026-02-19 10:04:14 +0530

    UC2:Hardcoded palindrome

--- src/PalidromeCheckerApp.java
+++ src/PalidromeCheckerApp.java
@@ -9,8 +9,34 @@ class Palindrome {
 }
 
 public class PalidromeCheckerApp {
+
+    static void checkPalindrome(String s) {
+        int size = s.length();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < size / 2; i++) {
+            if (s.charAt(i) != s.charAt(size - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string is a palindrome");
+        } else {
+            System.out.println("The string is NOT a palindrome");
+        }
+    }
+
     public static void main(String[] args) {
         Palindrome p = new Palindrome();
         p.start();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your string: ");
+        String s = sc.nextLine();
+
+        checkPalindrome(s);
+
     }
 }
